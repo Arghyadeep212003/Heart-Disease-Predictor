a32ab32e7f179cb6a4f63aeaf75985a8c16c8057
Arghyadeep Deb <debarghyadeep4@gmail.com>
2025-08-22 12:49:53 +0530

    Update settings.py

--- HeartDisease/settings.py
+++ HeartDisease/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m1!%e@fmk5vtl0+q1%1@*1r8dc%$g4b2vm#os_x6ch3ece6-==
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
